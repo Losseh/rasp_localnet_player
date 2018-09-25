9d7c6125531f3c78edd77ede5ae216401b1db84c
Losseh <aszymanski90@gmail.com>
2018-09-25 22:21:42 +0200

    remove volume control

--- player/app/routes.py
+++ player/app/routes.py
@@ -5,31 +5,65 @@ import datetime
 import subprocess
 import re
 
+class State:
+  def __init__(self):
+    self.radio_running = False
+    self.source1 = False
+
+state = State()
+
+
 @app.route('/')
 def index():
+  set_volume(100)
   templateData = get_index_arguments()
   return render_template('index.html', **templateData)
 
-@app.route('/volume_up/', methods=['POST'])
-def volume_up():
-  set_volume(saturate_percent(get_volume() + 5))
+@app.route('/run_radio/', methods=['POST'])
+def run_radio():
+  call_stop_music()
+  call_run_radio()
+  state.radio_running = True
+  state.source1 = False
   return redirect('/')
 
-@app.route('/volume_down/', methods=['POST'])
-def volume_down():
-  set_volume(saturate_percent(get_volume() - 5))
+@app.route('/run_source1/', methods=['POST'])
+def run_source1():
+  call_stop_music()
+  call_run_source1()
+  state.radio_running = False
+  state.source1 = True
   return redirect('/')
+
+@app.route('/stop_music/', methods=['POST'])
+def stop_radio():
+  call_stop_music()
+  state.radio_running = False
+  state.source1 = False
+  return redirect('/')
+
+def call_run_source1():
+  return subprocess.call([app_config['system_path'] + 'run_directory.sh', '/media/source1'])
+
+def call_run_radio():
+  return subprocess.call([app_config['system_path'] + 'run_radio.sh'])
+
+def call_stop_music():
+  return subprocess.call([app_config['system_path'] + 'stop_music.sh'])
 
 def get_index_arguments():
-  now = datetime.datetime.now()
-  timeString = now.strftime("%Y-%m-%d %H:%M")
-  volume = get_volume()
-  templateData = {
-    'title' : 'HELLO!',
-    'time': timeString,
-    'volume': volume
+  return {
+    'title': 'My home player',
+    'music_state': get_music_state(),
   }
-  return templateData
+
+def get_music_state():
+  if (state.radio_running):
+    return 'Radio paradise is running'
+  if (state.source1):
+    return 'Source1 is running'
+  else:
+    return 'Music is off'
 
 def set_volume(percent):
   print 'set ' + str(percent) + '%'
@@ -37,15 +71,6 @@ def set_volume(percent):
   amixer = percent_to_amixer(percent)
   ret_val = subprocess.call([cmd, amixer])
   return ret_val
-
-def get_volume():
-  cmd = app_config['system_path'] + 'get_volume.sh'
-  percent = subprocess.check_output([cmd])
-  percent = re.sub('\%', '', percent)
-  return int(percent)
-
-#def amixer_to_percent(amixer):
-#  return int((100. * (int(amixer) - app_config['min_vol'])) / (app_config['max_vol'] - app_config['min_vol']))
 
 def percent_to_amixer(percent):
   return str(int(percent * (app_config['max_vol'] - app_config['min_vol']) / 100 + app_config['min_vol']))
--- player/run_radio.sh
+++
@@ -1,1 +0,0 @@
-mplayer http://stream-uk1.radioparadise.com/mp3-192 </dev/null >/dev/null 2>&1 &
