5887d219e214f93561026de3846dbda80074cc9f
Losseh <aszymanski90@gmail.com>
2020-08-07 11:48:31 +0200

    [ASZ] add radio nowy swiat

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HOST="pi@192.168.0.52"
 ssh $HOST sudo pkill python
-ssh $HOST sudo pkill main.py
+ssh $HOST sudo pkill -9 main.py
 ssh $HOST sudo pkill mplayer
 
 ssh $HOST "rm -rf ~/player"
