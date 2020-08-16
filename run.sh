6bd039b8cc253170080b4a934612adf9fd2eb11a
Losseh <aszymanski90@gmail.com>
2020-08-16 09:50:02 +0200

    improve running docker image

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -t losseh/rasp_player . && docker run -d -p 80:5000 losseh/rasp_player
+docker build -t losseh/rasp_player . && \
+docker run -d -p 80:5000 --name rasp_player --device /dev/snd losseh/rasp_player
