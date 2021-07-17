8e9ee0d8bb04b3f7a3ed9ba9854805f193b47c7f
Losseh <aszymanski90@gmail.com>
2021-07-17 12:42:01 +0200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ ssh $HOST sudo pkill python
 ssh $HOST sudo pkill -9 main.py
 ssh $HOST sudo pkill mplayer
 
-ssh $HOST "rm -rf ~/player"
-scp -v -r player pi@192.168.0.52:~/.
+ssh $HOST "rm -rf ~/rasp_localnet_player/player"
+scp -v -r player pi@192.168.0.52:~/rasp_localnet_player/.
 
-ssh $HOST sudo "./player/main.py < /dev/null &> ~/player/log &"
+ssh $HOST sudo "./rasp_localnet_player/scripts/start.sh"
