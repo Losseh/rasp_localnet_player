f25d2f05ad28efbb5d1fa231bac08ffd7396378b
Losseh <aszymanski90@gmail.com>
2021-12-17 21:34:11 +0100

    fix tests

--- player/app/source/test_directory_reader.py
+++ player/app/source/test_directory_reader.py
@@ -2,7 +2,7 @@ from directory_reader import read_directories
 
 
 def test_read_directories():
-    expected = {'dir1', 'dir2'}
+    expected = {'dir1', 'dir2', 'dir with space'}
 
     actual = read_directories('./test_resources')
 
