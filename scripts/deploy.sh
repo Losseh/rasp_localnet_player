675f5a040fc8a395a820066a201b67b85e4fee84
Losseh <aszymanski90@gmail.com>
2022-01-29 11:38:54 +0100

    clean up deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,7 @@ HOST="pi@192.168.0.52"
 #todo move autodeploy to /home/pi/autodeploy.sh
 #run /home/pi/autodeploy.sh
 
-ssh $HOST sudo pkill python
-ssh $HOST sudo pkill -9 main.py
-ssh $HOST sudo pkill mplayer
+ssh $HOST sudo "./rasp_localnet_player/scripts/stop.sh"
 
 ssh $HOST "rm -rf ~/rasp_localnet_player/player"
 scp -v -r ../player pi@192.168.0.52:~/rasp_localnet_player/.
